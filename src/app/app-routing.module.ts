3478f217ab12fb64ce96a2a54ab8117b77d9bc5f
zahiravega <zayvega06@gmail.com>
2024-06-24 10:28:03 -0300

    importe inicio en app-routing.module.ts y cree las  cargas perezosas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './modules/inicio/components/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //ruta por defecto en la inicializacion 
+
+  {
+    path: "", component:InicioComponent
+  },
+
+  //ruta que vincula al modulo de incio y todo su contenido 
+  {
+    path: "", loadChildren: () => import('./modules/inicio/inicio.module').then(m => m.InicioModule)
+  },
+  {
+    path: "", loadChildren: () => import('./modules/producto/producto.module').then(m => m.ProductoModule)
+  },
+  {
+    path: "", loadChildren:()=> import('./modules/autentificacion/autentificacion.module').then(m => m.AutentificacionModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
