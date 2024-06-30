489b4e277b23beb6bce36fd0d499f8e9732fc5da
zahiravega <zayvega06@gmail.com>
2024-06-30 19:14:55 -0300

    intalacion de firebase

---
+++ src/enviroments/enviroment.ts
@@ -0,0 +1,13 @@
+//configuracion de firebase en la pagina
+export const enviroment = {
+    production:false,
+
+firebaseConfig: {
+    apiKey: "AIzaSyCC43wZCqe1Y2VcjNVoZLa_sH8HOuc7XTY",
+    authDomain: "afrodita-lingerie.firebaseapp.com",
+    projectId: "afrodita-lingerie",
+    storageBucket: "afrodita-lingerie.appspot.com",
+    messagingSenderId: "506475318032",
+    appId: "1:506475318032:web:f035f6522268ef5e33f395"
+  }
+}
