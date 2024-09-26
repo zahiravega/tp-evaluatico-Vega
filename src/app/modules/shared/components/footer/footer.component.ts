8f32744b8cd1c0bed7a9d70128fc3beddceb05e4
zahiravega <zayvega06@gmail.com>
2024-09-26 00:51:10 -0300

    footer terminado y responsivo

--- src/app/modules/shared/components/footer/footer.component.ts
+++ src/app/modules/shared/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  cols: number = 3;  // Número de columnas por defecto
 
+  constructor() {
+    this.adjustGridCols(window.innerWidth);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.adjustGridCols(event.target.innerWidth);
+  }
+
+  adjustGridCols(width: number) {
+    if (width <= 600) {
+      this.cols = 1;  // 1 columna en pantallas pequeñas
+    } else if (width > 600 && width <= 960) {
+      this.cols = 2;  // 2 columnas en pantallas medianas
+    } else {
+      this.cols = 3;  // 3 columnas en pantallas grandes
+    }
+  }
 }
